d30cee399d09423aedb8cd652c5e117c75b459f1
David Dong <hidaviddong@gmail.com>
2025-05-20 14:08:22 +0800

    feat: button

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <Button>Click me</Button>
-      <Button className="font-bold">Click me</Button>
+      <Button disabled>Click me</Button>
     </div>
   );
 }
